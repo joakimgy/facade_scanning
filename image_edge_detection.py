22e5373d3a6976d38cc77e0a4e19b5da3433fc39
pzon <andre.perzon@hotmail.com>
2018-10-27 19:54:49 +0200

    fixat edge detection setting och testat alla bilder

--- image_edge_detection.py
+++ image_edge_detection.py
@@ -4,20 +4,39 @@ import math
 import sys
 from matplotlib import pyplot as plt
 
-inputimage ='img/red_wall.jpg'
+inputimage ='img/big_building.jpg'
 
-
+orig_image = cv2.imread(inputimage)
 img = cv2.imread(inputimage,0)
+blurred = cv2.GaussianBlur(img, (3, 3), 0)
 
-# Canny edge detection
-edges = cv2.Canny(img, 100, 200)
 
+#---- apply automatic Canny edge detection using the computed median----
+v = np.median(img)
+sigma=0.33
+lower = int(max(0, (1.0 - sigma) * v))
+upper = int(min(255, (1.0 + sigma) * v))
+
+#----- Otsu method for edge detection
+high_thresh, thresh_im = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+lowThresh = 0.5*high_thresh
+
+# Canny edge detection with blurred image
+blurrededges = cv2.Canny(blurred, lower, upper)
+# canny edge detection with gray scaled image
+greyedges = cv2.Canny(img, lower, upper)
 
 
 # Hough lines
-minLineLength = 100
-maxLineGap = 10
-lines = cv2.HoughLinesP(image=edges,rho=1,theta=np.pi/180, threshold=15,
+#min for redwall = 35, max = 200
+#min for close_up = 5, max = 1500 this settings works on redwall and building also
+#min for brick_wall2 = 190, max 200
+#glas_building min = 10, max = 300
+#whole_building min =10, max = 40
+#big_building min 10, max 1400
+minLineLength = 10
+maxLineGap = 1400
+lines = cv2.HoughLinesP(image=greyedges,rho=1,theta=np.pi/180, threshold=100,
 lines=np.array([]),minLineLength=minLineLength,maxLineGap=maxLineGap)
 if lines is not None:
 	no_lines=0
@@ -28,24 +47,18 @@ if lines is not None:
 		    angleDeg = abs(math.atan2(y2-y1,x2-x1)*180/np.pi)
 		    # Only include line with angles > 80 degrees (almost vertical)
 		    if angleDeg > 80:
-			cv2.line(img,(x1,y1),(x2,y2),(0,0,255),4)
+			cv2.line(orig_image,(x1,y1),(x2,y2),(0,0,255),10)
 			no_lines = no_lines + 1
 	print "Number of vertical lines: %d" % no_lines
 
 # Show images
 cv2.namedWindow('Hough',cv2.WINDOW_NORMAL)
 cv2.namedWindow('Canny',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Hough', 1920,1080)
-cv2.resizeWindow('Canny', 1920,1080)
-cv2.imshow('Hough',img)
-cv2.imshow('Canny', edges)
+cv2.resizeWindow('Hough', 720,720/16*9)
+cv2.resizeWindow('Canny', 720,720/16*9)
+cv2.imshow('Hough', orig_image)
+cv2.imshow('Canny', greyedges)
 
-#plt.subplot(121),plt.imshow(img,cmap = 'gray')
-#plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-#plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-#plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
-#plt.show()
 cv2.waitKey(0)
 	
 
